e709fb0606b14cfd7e275842f1c052c91f3783b2
EvelynKyomugisha <evelynkyomugisha12@gmail.com>
2024-09-20 20:22:35 +0300

    Add files via upload

---
+++ Kyomugisha Evelyne.php
@@ -0,0 +1,18 @@
+<?php
+$name="Kyomugisha Evelyne";
+$dob="20-8-2000";
+$address="-kabale";
+$YearA=2024;
+$YearB=2000;
+$age=$YearA-$YearB;
+
+echo "welcome home ","$name " ," $address.","you are"," $age" ,"years of age";
+
+
+
+
+
+
+
+
+?>
